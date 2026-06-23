d7a053a14241cfad7b3f5599074445549a3f7bd0
Stefani Gabriele Arnold de Camargo <stefaniqrnold@gmail.com>
2026-06-23 13:15:24 -0300

    Add XML export functionality to XMLTransformer
    Implemented XML export functionality using DBManager to retrieve records and XMLHandler to create and write the XML document.

--- src/br/edu/unijui/pcn/logic/XMLTransformer.java
+++ src/br/edu/unijui/pcn/logic/XMLTransformer.java
@@ -1,21 +1,98 @@
 package br.edu.unijui.pcn.logic;
 
+import br.edu.unijui.pcn.utils.XMLHandler;
+import java.sql.SQLException;
+import java.util.List;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import java.util.logging.Level;
 
-/**
- * Esta classe implementa a lógica de negócio para ler e gerar um arquivo XML 
- * com os dados de isolamento social.
- * @author <seu nome aqui>
- */
 public class XMLTransformer {
-
-    /**
-     * Exporta todos os dados de isolamento social carregados no banco de dados 
-     * para um arquivo com nome especificado no parâmetro do método
-     * @param fileName indica o nome do arquivo ao qual devem ser exportados os dados. 
-     */
-    public void export(String fileName) {
-            
-        
-    }
     
+    
+    private final DBManager dbManager;
+
+    public XMLTransformer(DBManager dbManager) {
+        this.dbManager = dbManager;
+    }
+
+    public void export(String fileName) {
+
+        try {
+
+            List<IsolationRecord> records =
+                    dbManager.getAllRecords();
+
+            Document doc =
+                    XMLHandler.newDocument();
+            
+            if (doc == null) {
+                throw new RuntimeException(
+                    "Não foi possível criar o documento XML."
+                );
+            }
+
+            // Elemento raiz
+            Element root =
+                    doc.createElement("isolation-indexes");
+
+            doc.appendChild(root);
+
+            int id = 1;
+
+            for (IsolationRecord record : records) {
+
+                Element covid =
+                        doc.createElement("covid");
+
+                covid.setAttribute(
+                        "id",
+                        String.valueOf(id++)
+                );
+
+                covid.setAttribute(
+                        "city",
+                        record.city()
+                );
+
+                covid.setAttribute(
+                        "state-name",
+                        record.state()
+                );
+
+                covid.setAttribute(
+                        "state-acronym",
+                        record.stateAcronym()
+                );
+
+                covid.setAttribute(
+                        "date",
+                        record.date()
+                );
+
+                covid.setAttribute(
+                        "index",
+                        String.valueOf(record.index())
+                );
+
+                root.appendChild(covid);
+            }
+
+            if (!fileName.endsWith(".xml")) {
+                fileName += ".xml";
+            }
+
+            XMLHandler.writeXmlFile(doc, fileName);
+
+                    LogManager.getLogger().info("Arquivo XML exportado: "+ fileName);
+
+        } catch (SQLException e) {
+
+            LogManager.getLogger().log(
+            Level.WARNING,
+            "Erro ao exportar XML",
+            e
+        );
+        }
+    }
 }
